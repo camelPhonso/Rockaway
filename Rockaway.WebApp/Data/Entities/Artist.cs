eab33b0ceffde56d6a66d92c1a180d14a36ab717
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-03-29 10:56:03 +0000

    test: added sample data

--- Rockaway.WebApp/Data/Entities/Artist.cs
+++ Rockaway.WebApp/Data/Entities/Artist.cs
@@ -4,16 +4,16 @@ using Microsoft.EntityFrameworkCore;
 namespace Rockaway.WebApp.Data.Entities;
 
 public class Artist {
-	public Guid Id { get; set; }
+	public Guid Id {get;set;}
 	[MaxLength(100)]
-	public string Name { get; set; } = String.Empty;
+	public string Name {get;set;} = String.Empty;
 	[MaxLength(500)]
-	public string Description { get; set; } = String.Empty;
+	public string Description {get;set;} = String.Empty;
 	[MaxLength(100)]
 	[Unicode(false)]
-	public string Slug { get; set; } = String.Empty;
-	public Artist() { }
-	public Artist(Guid id, string name, string description, string slug) {
+	public string Slug{get;set;} = String.Empty;
+	public Artist(){}
+	public Artist(Guid id, string name, string description, string slug){
 		this.Id = id;
 		this.Name = name;
 		this.Description = description;
