107dfc3fe8c2b1938f8c8fe6ecb4ac92c2b94b56
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-03-29 18:56:27 +0000

    feat: added UI navigation to /Artists

--- Rockaway.WebApp/obj/Debug/net8.0/Rockaway.WebApp.AssemblyInfo.cs
+++ Rockaway.WebApp/obj/Debug/net8.0/Rockaway.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rockaway.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc872022e0391bf8c80b4983e88f6c2ce7082b7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d797a69dc032d469ce12dd108f9b0be53060e3e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rockaway.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rockaway.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
