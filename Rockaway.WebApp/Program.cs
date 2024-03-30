01880f06ecf1d49443bfd4e903802acb9d784ea8
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-03-30 11:45:40 +0000

    build: manage new environments configuration

--- Rockaway.WebApp/Program.cs
+++ Rockaway.WebApp/Program.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Roackaway.WebApp.Services;
 using Rockaway.WebApp.Data;
+using Roackaway.WebApp.Hosting;
+using Roackaway.WebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,34 +16,67 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<RockawayDbContext>();
 
-var sqliteConnection = new SqliteConnection("Data Source=:memory:");
-sqliteConnection.Open();
-builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlite(sqliteConnection));
+// Create Loggers
+ILogger<T> CreateAdHocLogger<T>()
+{
+	var config = new ConfigurationBuilder()
+		.AddJsonFile("appsettings.json", false, true)
+		.AddEnvironmentVariables()
+		.AddJsonFile(
+			$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+			true,
+			true
+		)
+		.Build();
+	return LoggerFactory.Create(lb => lb.AddConfiguration(config)).CreateLogger<T>();
+}
+
+var logger = CreateAdHocLogger<Program>();
+
+logger.LogInformation(
+	"Rockaway is running on {environment} environment",
+	builder.Environment.EnvironmentName
+);
+
+if (HostEnvironmentExtentions.UseSqlite(builder.Environment))
+{
+	logger.LogInformation("Using Sqlite Database");
+	var sqliteConnection = new SqliteConnection("Data Source=:memory:");
+	sqliteConnection.Open();
+	builder.Services.AddDbContext<RockawayDbContext>(options =>
+		options.UseSqlite(sqliteConnection)
+	);
+}
+else
+{
+	logger.LogInformation("Using SQL Server database");
+	var connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+	builder.Services.AddDbContext<RockawayDbContext>(options =>
+		options.UseSqlServer(connectionString)
+	);
+}
 
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 var app = builder.Build();
 
-// Create Loggers
-app.Logger.LogTrace("This is a TRACE message.");
-app.Logger.LogDebug("This is a DEBUG message.");
-app.Logger.LogInformation("This is an INFORMATION message.");
-app.Logger.LogWarning("This is a WARNING message.");
-app.Logger.LogError("This is an ERROR message.");
-app.Logger.LogCritical("This is a CRITICAL message.");
-
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
 	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
+}
+else
+{
+	app.UseDeveloperExceptionPage();
 }
 
 using (var scope = app.Services.CreateScope())
 {
 	using (var db = scope.ServiceProvider.GetService<RockawayDbContext>()!)
 	{
-		db.Database.EnsureCreated();
+		if(HostEnvironmentExtentions.UseSqlite(app.Environment)){
+			db.Database.EnsureCreated();
+		}
 	}
 }
 
